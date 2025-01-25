c0507bc458064d9da954899fde128d48e36f4baf
Brian Stark <BStarkit@gmail.com>
2025-01-25 23:05:57 +0000

    update

--- single-script/temp.js
+++
@@ -1,7 +0,0 @@
-/** @param {NS} ns */
-export async function main(ns) {
-const upgradeName = "Sell for Money"
-        while (ns.hacknet.numHashes() > ns.hacknet.hashCost(upgradeName)) {
-            ns.hacknet.spendHashes(upgradeName);
-        }
-}
--- single-script/tset.js
+++
@@ -1,5 +0,0 @@
-/** @param {NS} ns */
-export async function main(ns) {
-  var ownedservers = ns.getServerMaxRam('home');
-ns.tprint(ownedservers)
-}
